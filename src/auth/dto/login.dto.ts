a87214afe7c318d8d5684e9584a2e65634eed123
Yorth H. Ortegon <yorth@udenar.edu.co>
2025-03-06 19:38:43 -0500

    feat: map swagger in login

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,4 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class LoginDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
   identification: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
