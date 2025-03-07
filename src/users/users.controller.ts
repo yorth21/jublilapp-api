c2321f7d1fe3c842cdcdb8b0d51a07ca34274a0c
Yorth H. Ortegon <yorth@udenar.edu.co>
2025-03-06 23:04:39 -0500

    feat: add public to create user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,12 +2,14 @@ import { Controller, Get, Post, Body, Patch, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Public } from 'src/auth/decorators/public.decorator';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @Public()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
