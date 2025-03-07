9656e9a9fb9d4bd644cf7f1611ad82b67ff42f4d
Yorth H. Ortegon <yorth@udenar.edu.co>
2025-03-07 14:39:46 -0500

    fix: add all-exceptions.filter.ts

---
+++ src/core/errors/all-exceptions.filter.ts
@@ -0,0 +1,40 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class AllExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    if (exception instanceof HttpException) {
+      console.log(exception);
+      const status = exception.getStatus();
+      const responseObj = exception.getResponse() as {
+        message: string | string[];
+      };
+
+      response.status(status).json({
+        statusCode: status,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+        message: responseObj.message,
+      });
+    } else {
+      console.error(exception);
+      response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+        message: 'Internal server error',
+      });
+    }
+  }
+}
