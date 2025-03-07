63b1755c5aa46b045b717c10dd722ff72a5021cb
Yorth H. Ortegon <yorth@udenar.edu.co>
2025-03-06 23:04:16 -0500

    feat: read .env in prisma.service

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -3,7 +3,21 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL,
+        },
+      },
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
